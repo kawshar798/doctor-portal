72876126c830e120f2532979310e7a98609a2bd0
“Kawsahr <kawshar798@gmail.com>
2020-07-08 21:31:41 +0600

    update active menu

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
@@ -6,6 +6,8 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from '@material-ui/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import {Link} from "react-router-dom";
+import { Button } from '@material-ui/core';
 
 
 
@@ -42,6 +44,11 @@ function ElevationScroll(props) {
         customNav:{
             background:'#fff'
         },
+        logoContainer:{
+          "&:hover":{
+            backgroundColor:"transparent"
+          }
+        }
         
       })
   )
@@ -51,22 +58,37 @@ const Header = () => {
     const classes = useStyles();
 
     const [value,setValue] = useState(0);
+
     const handleChange = (e,value) =>{
         setValue(value)
     }
+
+    useEffect(() => {
+      if(window.location.pathname === "/" && value !== 0){
+        setValue(0)
+      }else if(window.location.pathname === "/service" && value !== 1){
+        setValue(1)
+      }else if(window.location.pathname === "/serviceb" && value !== 2){
+        setValue(2) 
+      }
+     
+    }, [value]);
+
     return (
         <>
           <ElevationScroll>
         <AppBar position="fixed" className={classes.customNav}>
             <Toolbar disableGutters={true}>
-                <Typography variant="h3" color="secondary">Doctor Portal</Typography>
+              <Button className={classes.logoContainer} component={Link} to="/" onClick={ () =>setValue(0)} disableRipple>
+              <Typography variant="h3" color="secondary">Doctor Portal</Typography>
+              </Button>
                 
                 <Tabs value={value} onChange={handleChange} className={classes.tabContainer}
                 indicatorColor="secondary"
                 >
-                    <Tab className={classes.tab}  label="Home"/>
-                    <Tab className={classes.tab} label="About"/>
-                    <Tab className={classes.tab} label="Dental Services"/>
+                    <Tab className={classes.tab} component={Link} to="/"  label="Home"/>
+                    <Tab className={classes.tab} component={Link} to="/service" label="About"/>
+                    <Tab className={classes.tab} component={Link} to="/serviceb" label="Dental Services"/>
                     <Tab className={classes.tab} label="Reviews"/>
                     <Tab className={classes.tab} label="Blog"/>
                     <Tab className={classes.tab} label="Contact Us"/>
